7c2682f089d7dec8f8e51a86aaf86a63f2ad46a0
silencemind <silencemind55@gmail.com>
2020-12-10 23:52:14 +0500

    changes

--- module1/part1/01.portscanner/src/portscanner.py
+++ module1/part1/01.portscanner/src/portscanner.py
@@ -4,25 +4,30 @@ import socket
 
 
 def get_accessible_ports(address, min_port, max_port):
-	found_ports = []
+    found_ports = []
+    add = socket.gethostbyname(address)
+    # write code here
+    for port in range(min_port,max_port-1):
+        conn = socket.socket()
+        r = conn.connect_ex((add,port))
+        if r == 0: 
+            found_ports.append((port))
+            conn.close()
+    return found_ports
 
-	# write code here
-
-	return found_ports
 
 
 def main(argv):
-	address = sys.argv[1]
-	min_port = int(sys.argv[2])
-	max_port = int(sys.argv[3])
-	ports = get_accessible_ports(address, min_port, max_port)
-	for p in ports:
-		print(p)
-
+    address = sys.argv[1]
+    min_port = int(sys.argv[2])
+    max_port = int(sys.argv[3])
+    ports = get_accessible_ports(address, min_port, max_port)
+    for p in ports:
+        print(p)
 # This makes sure the main function is not called immediatedly
 # when TMC imports this module
 if __name__ == "__main__": 
-	if len(sys.argv) != 4:
-		print('usage: python %s address min_port max_port' % sys.argv[0])
-	else:
-		main(sys.argv)
+    if len(sys.argv) != 4:
+        print('usage: python %s address min_port max_port' % sys.argv[0])
+    else:
+        main(sys.argv)
