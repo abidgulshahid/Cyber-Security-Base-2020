53defdbbce37ae1df2d57db0e81627ae890b4bb7
silencemind <silencemind55@gmail.com>
2020-12-11 00:56:39 +0500

    Final Version

--- module1/part1/01.portscanner/src/portscanner.py
+++ module1/part1/01.portscanner/src/portscanner.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 import sys
 import socket
-
-
+ 
+ 
 def get_accessible_ports(address, min_port, max_port):
     found_ports = []
     add = socket.gethostbyname(address)
-    # write code here
-    for port in range(min_port,max_port-1):
+    for port in range(min_port,max_port+1):
         conn = socket.socket()
         r = conn.connect_ex((add,port))
         if r == 0: 
             found_ports.append((port))
             conn.close()
+ 
     return found_ports
-
-
-
+ 
+ 
+ 
 def main(argv):
     address = sys.argv[1]
     min_port = int(sys.argv[2])
     max_port = int(sys.argv[3])
     ports = get_accessible_ports(address, min_port, max_port)
-    for p in ports:
-        print(p)
+    if ports:
+        for p in ports:
+ 
+            print("Founded Ports: ",str(p))
+    else:
+        print ("All Ports are closed")
 # This makes sure the main function is not called immediatedly
 # when TMC imports this module
 if __name__ == "__main__": 
